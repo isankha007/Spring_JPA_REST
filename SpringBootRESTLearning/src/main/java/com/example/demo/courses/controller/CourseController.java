c5ade675a8b288d0e60154524dd463984556d6f9
isankha007 <sankhadeep2007@gmail.com>
2022-02-10 18:02:47 +0530

    delete post added

--- SpringBootRESTLearning/src/main/java/com/example/demo/courses/controller/CourseController.java
+++ SpringBootRESTLearning/src/main/java/com/example/demo/courses/controller/CourseController.java
@@ -1,12 +1,15 @@
 package com.example.demo.courses.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.courses.bean.Course;
@@ -33,5 +36,21 @@ public class CourseController {
 		return course.get();
 		//return new Course(1,"sankha","Learn microservices 1");
 	}
-
+	
+	@PostMapping("/courses")
+	public void createCourse(@RequestBody Course course) {
+		repository.save(course);
+		
+	}
+	
+	@PutMapping("/courses/{id}")
+	public void updateCourse(@PathVariable long id,@RequestBody Course course) {
+		repository.save(course);
+		
+	}
+	@DeleteMapping("/courses/{id}")
+	public void deleteCourse(@PathVariable long id) {
+		repository.deleteById(id);
+		
+	}
 }
